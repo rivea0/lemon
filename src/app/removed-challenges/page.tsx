030518de09a435b12014200d7f3497d6fee11373
rivea0 <58330360+rivea0@users.noreply.github.com>
2024-01-10 04:49:31 +0300

    Fix heading showing when there is no removed challenges

--- src/app/removed-challenges/page.tsx
+++ src/app/removed-challenges/page.tsx
@@ -10,26 +10,29 @@ export default async function RemovedChallenges() {
 
   return (
     <div className="mt-8">
-      <h1 className="text-xl text-center">Removed Challenges</h1>
       {!removedChallenges.every((i) => !i) ? (
-        <ul className="grid grid-cols-3 mt-8 gap-4 px-16 portrait:grid-cols-1 portrait:px-4">
-          {removedChallenges.map(
-            (c) =>
-              c && (
-                <li
-                  className={`border rounded-md py-4 px-8 text-center ${
-                    gradientColors[c.id_color]
-                  } text-black`}
-                  key={c.id}
-                >
-                  <p className={`text-xl`}>{c.title}</p>
-                  <form action={deleteChallenge} className="mt-8 text-sm">
-                    <RemoveButton id={c.id.toString()} isToDelete />
-                  </form>
-                </li>
-              )
-          )}
-        </ul>
+        <>
+          <h1 className="text-xl text-center">Removed Challenges</h1>
+
+          <ul className="grid grid-cols-3 mt-8 gap-4 px-16 portrait:grid-cols-1 portrait:px-4">
+            {removedChallenges.map(
+              (c) =>
+                c && (
+                  <li
+                    className={`border rounded-md py-4 px-8 text-center ${
+                      gradientColors[c.id_color]
+                    } text-black`}
+                    key={c.id}
+                  >
+                    <p className={`text-xl`}>{c.title}</p>
+                    <form action={deleteChallenge} className="mt-8 text-sm">
+                      <RemoveButton id={c.id.toString()} isToDelete />
+                    </form>
+                  </li>
+                )
+            )}
+          </ul>
+        </>
       ) : (
         <div className="flex flex-col items-center">
           <h1 className="text-xl">You have no removed challenges.</h1>
